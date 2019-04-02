f33ea9e6f02a2837030440a35e212f562295801e
Aaron Gerig <a.gerig@w-vision.ch>
2019-04-02 14:26:43 +0200

    Completed the course

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 Vue.use(vuelidate.default);
 
+const pizzaOrBurger = value => value === 'pizza' || value === 'burger' || !validators.helpers.req(value);
+const oldEnoughAndAlive = validators.between(12, 120);
+
 new Vue({
   el: '#app',
 
@@ -7,7 +10,11 @@ new Vue({
     return {
       form: {
         name: null,
-        age: null
+        age: null,
+        newsletter: null,
+        email: null,
+        githubUsername: null,
+        food: null
       }
     };
   },
@@ -20,13 +27,39 @@ new Vue({
       age: {
         required: validators.required,
         integer: validators.integer,
-        between: validators.between(12, 120)
+        oldEnoughAndAlive
+      },
+      email: {
+        email: validators.email,
+        required: validators.requiredIf(function () {
+          return !!this.form.newsletter;
+        })
+      },
+      githubUsername: {
+        exists(value) {
+          if (!validators.helpers.req(value)) {
+            return true;
+          }
+
+          return axios.get(`//api.github.com/users/${value}`);
+        }
+      },
+      food: {
+        pizzaOrBurger
       }
     }
   },
 
   methods: {
+    shouldAppendErrorClass(field) {
+      return field.$error;
+    },
+    shouldAppendValidClass(field) {
+      return !field.$invalid && field.$model && field.$dirty;
+    },
     submitForm() {
+      this.$v.form.$touch();
+
       if (!this.$v.form.$invalid) {
         console.log('📝 Form submitted', this.form);
       } else {
